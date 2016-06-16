33036bf15a1c40e4399f1e7afd45f8752bf72bc3
Guilherme Victor <guivictor2011@gmail.com>
2016-06-15 22:53:41 -0300

    Implementação agendamento [nao terminado]

--- app/static_content/js/webapp/scheduler/scheduler-card.js
+++ app/static_content/js/webapp/scheduler/scheduler-card.js
@@ -5,9 +5,7 @@ function initCommandControls() {
 		var command = $(this).attr('data-command');
 		
 		var data = {};
-		data.command = command;
-		
-		//TODO				
+		data.command = command;			
 		
 		$.ajax({
 			type: 'POST',
@@ -35,13 +33,62 @@ function initCommandControls() {
 		
 		$modal.modal('show');
 		
+		$($modal).find('.schedule-command-btn-day').removeClass('btn-danger');
+		
+		
+		$modal.find('.schedule-command-btn-day').unbind('click');
+		$modal.find('.schedule-command-btn-day').click(function (e) {
+			e.preventDefault();
+			
+			var $hidden = $(this).find('.schedule-command-day');
+			
+			if ($($hidden).val() == 'true') {
+				$($hidden).val('false');
+				$(this).removeClass('btn-danger');
+			}
+			else {
+				$($hidden).val('true');
+				$(this).addClass('btn-danger');
+			}
+		});
+		
 		$modal.find('.btn-schedule-command').unbind('click');
-		$modal.find('.btn-schedule-command').click(function () {
+		$modal.find('.btn-schedule-command').click(function (e) {
 			e.preventDefault();
 		
-			var command = $(this).attr('data-command');
+			var data = {};
+						
+			var command = $(this).attr('data-command');			
+			data.command = command;
 			
-			//TODO
+			data.repeat = new Array();
+			$($modal).find('.schedule-command-day').each(function () {
+				
+				var day = {};
+				
+				day.name = $(this).attr('data-day');
+				day.checked = $(this).val() == 'true';
+				
+				data.repeat.push(day);							
+			});
+			
+			console.log(data);			
+			return;
+			$.ajax({
+				type: 'POST',
+				url: '/schedulecommand',
+				async: false,
+				dataType: 'json',
+				contentType: 'application/json',
+				data: JSON.stringify(data),
+				error: function(err) {
+					console.log(err);
+					//TODO alert
+				},
+				success: function (data) {
+					console.log('ok');
+				}
+			});
 			
 		});
 	});
